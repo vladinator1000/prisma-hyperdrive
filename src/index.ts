960fa30b8d4cb9fa2971fe3ca18ccdb208fdf860
Vlady Veselinov <vladkodmc@gmail.com>
2024-03-13 18:40:07 +0000

    clean up comments

--- src/index.ts
+++ src/index.ts
@@ -6,16 +6,16 @@ export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
 		let num = new Prisma.Decimal(1.7).add(1).mul(1).div(1).sub(0.01).abs(); // this works
 
-		// All errors below
-		num.round(); // round is not a function
-		num.sin();
-		num.cos();
+		// All of the rows below throw a type error
+		num.round(); // not a function
+		num.sin(); // not a function
+		num.cos(); // not a function
 
 		// Static methods also broken
-		Prisma.Decimal.round(5); //  is not a function
-		Prisma.Decimal.add(5, 3); // is not a function
-		Prisma.Decimal.mul(5, 3); // is not a function
-		Prisma.Decimal.sub(5, 3); // is not a function
+		Prisma.Decimal.round(5); // not a function
+		Prisma.Decimal.add(5, 3); // not a function
+		Prisma.Decimal.mul(5, 3); // not a function
+		Prisma.Decimal.sub(5, 3); // not a function
 
 		// This list is not exhaustive, I haven't checked all the methods
 
