cdfa99eaa03f71bb755fb502f3d675b8730f9e9d
Vlady Veselinov <vladkodmc@gmail.com>
2024-03-13 18:36:11 +0000

    show missing methods

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,25 @@
 import { PrismaClient, Prisma } from '@prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg';
-import pg from 'pg';
-
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
 
 export interface Env {
-	HYPERDRIVE: Hyperdrive;
 	DATABASE_URL: string;
 }
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		// const pool = new pg.Pool({
-		// 	connectionString: env.DATABASE_URL,
-		// 	max: 1,
-		// });
-		// const adapter = new PrismaPg(pool);
-		// const prisma = new PrismaClient({ adapter } as any);
+		let num = new Prisma.Decimal(1.7).add(1).mul(1).div(1).sub(0.01).abs(); // this works
 
-		// const res = await prisma.$queryRaw`select 1+1`;
+		// All errors below
+		num.round(); // round is not a function
+		num.sin();
+		num.cos();
 
-		const num = new Prisma.Decimal(1.7).round();
+		// Static methods also broken
+		Prisma.Decimal.round(5); //  is not a function
+		Prisma.Decimal.add(5, 3); // is not a function
+		Prisma.Decimal.mul(5, 3); // is not a function
+		Prisma.Decimal.sub(5, 3); // is not a function
+
+		// This list is not exhaustive, I haven't checked all the methods
 
 		return new Response(num.toString());
 	},
